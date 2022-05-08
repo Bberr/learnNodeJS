b1a2612858ff17ee5a34df70afecb0bef680238b
Bberr <63011212003@msu.ac.th>
2022-05-09 01:59:05 +0700

    Update learn.js

--- learn.js
+++ learn.js
@@ -13,7 +13,26 @@ const customerName = "big"
 const age = 20
 const address = "มหาสารคาม"
 
-const customer = {customerName : customerName,age,address}//ในกรณีที่จะเอาatribliteมาเป็นข้อมูลในobjectสามารถเขียนแบบนี้ได้
+const customer = {
+    customerName : customerName,age,address,//ในกรณีที่จะเอาatribliteมาเป็นข้อมูลในobjectสามารถเขียนแบบนี้ได้
+
+    showData(){
+        console.log("ชื่อผู้ใช้ : "+customerName);
+    }
+}
+//String
+const address2 = "เกียรตินำชัย อินทร์นอก ที่อยู่ 206 หมู่ 1 ต.หินดาด อ.ห้วยแถลง จ.นครราชสีมา เบอร์โทรติดต่อ 0983395822"
+const address3 = `เกียรตินำชัย อินทร์นอก
+                ที่อยู่ 206 หมู่ 1 ต.หินดาด อ.ห้วยแถลง จ.นครราชสีมา 
+                เบอร์โทรติดต่อ 0983395822` //ขึ้นบรรทัดข้อความยาวๆ
+    //ตัวแปรต่อString
+let name = "เกียรตินำชัย อินทร์นอก"
+let telphone = "0983395822"
+const address4 = `${name}
+                ที่อยู่ 206 หมู่ 1 ต.หินดาด อ.ห้วยแถลง จ.นครราชสีมา 
+                เบอร์โทรติดต่อ ${telphone}`
+
 //main
-console.log(name("big","innork"))
+console.log(name("big","innork"));
 console.log(customer);
+customer.showData();
